1e45fbe355830fe02c0f064ccec8eefd418035d4
George Parapanissios <frontend.dev@realitize.ai>
2025-12-16 23:20:26 +0200

    feat: add Vercel Analytics integration and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Roboto, Roboto_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import Footer from "@/components/footer";
 
 import "./globals.css";
@@ -37,6 +38,7 @@ export default function RootLayout({
       >
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
